18493a5f9d38c9ea512c55dc0ac37ddb4c8287cc
Andreas Schärtl <andreas@schaertl.me>
2022-04-09 19:59:11 +0200

    dump-wiktionary-to-sqlite.sh: compress created image w/ gzip

--- scripts/dump-wiktionary-to-sqlite.sh
+++ scripts/dump-wiktionary-to-sqlite.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 
 #
 # dump-wiktionary-to-sqlite.sh
@@ -7,12 +7,15 @@
 # Creates and potentially overwrites a file enwiktionary-latest-pages-articles.sqlite3
 # in the current working directory.
 #
-# You will need curl, bunzip and wdictosqlite in your PATH.
+# You will need curl, bunzip, gzip and wdictosqlite in your PATH.
 #
 
-set -eu
+set -euo pipefail
 
 source_addr=https://dumps.wikimedia.org/enwiktionary/latest/enwiktionary-latest-pages-articles.xml.bz2
 outfile=enwiktionary-latest-pages-articles.sqlite3
 
-curl --silent $source_addr | bunzip2 | wdictosqlite -outfile $outfile
+curl --silent "$source_addr" | bunzip2 | wdictosqlite -outfile "$outfile"
+gzip "$outfile"
+
+echo "$0: created database at $outfile.gz" 1>&2
